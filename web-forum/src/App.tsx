a8caf5379823554ddbda74ac6cd95c2501dd5839
User <user@example.com>
2025-12-30 16:04:38 +0800

    initial project setup

---
+++ web-forum/src/App.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+  const topics = ["React", "Go", "Databases"]
+
+  function increment() {
+    setCount(prev => prev + 5)
+  }
+
+  function form(e: any) {
+    e.preventDefault();
+    setText(e.target.value)
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <input
+        value = {text}
+        onChange={form}
+      />
+      <ul>
+        {topics.map(topic => (
+          <li>{topic}</li>
+        ))}
+      </ul>
+      <div className="card">
+        <button onClick={increment}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
